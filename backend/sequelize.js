ea658f2723cfaacef4271e45800dd18ba16e8595
satwik <satwiknayyar.2005@gmail.com>
2026-03-26 16:03:25 +0530

    Fixed postgres connection

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -1,16 +1,15 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config({ path: './config.env' });
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-module.exports = sequelize; 
+module.exports = sequelize;
